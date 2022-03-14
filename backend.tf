364df291eca52cf1e92c990c19bf53fdb310ca74
Josh Johanning <joshjohanning@github.com>
2022-03-14 11:29:34 -0500

    running pre-commit run -a

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization  = "joshjohanning-org"
+    organization = "joshjohanning-org"
     workspaces {
       name = "my-app" # name for your app's state.
     }
