6176cc9489241dc153a1377b15df2bbe485d3e3a
Josh Johanning <joshjohanning@github.com>
2022-03-14 14:41:49 -0500

    removing slot

--- appservice.tf
+++ appservice.tf
@@ -26,19 +26,3 @@ resource "azurerm_app_service" "app" {
     use_32_bit_worker_process = true
   }
 }
-
-# resource "azurerm_app_service_slot" "slotDemo" {
-#   name                = var.app_service_slot
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   app_service_plan_id = azurerm_app_service_plan.asp.id
-#   app_service_name    = azurerm_app_service.app.name
-
-#   site_config {
-#     linux_fx_version         = "DOTNETCORE|3.1"
-#     remote_debugging_enabled = false
-#     remote_debugging_version = "VS2019"
-#     http2_enabled            = true
-#     always_on                = true
-#   }
-# }
